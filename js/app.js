c5a70b948dc4f40cce301d1d94e379abe133889a
Louis Roy <louisroy@louisroy.net>
2013-11-05 16:09:02 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,215 @@
+var App = (function () {
+
+	var $map = null;
+	var $console = null;
+	var $search = null;
+
+	var map = null;
+	var markers = [];
+	var geocoder = null;
+
+	var $searchBtn = null;
+	var $clearBtn = null;
+	var $exportBtn = null;
+
+	$.fn.getAttributes = function () {
+		var attributes = {};
+
+		if (this.length) {
+			$.each(this[0].attributes, function (index, attr) {
+				attributes[ attr.name ] = attr.value;
+			});
+		}
+
+		return attributes;
+	};
+
+	var onAppReady = function (ev) {
+		console.log('App ready');
+
+		$map = $('#map');
+		$console = $('#console');
+		$search = $('#search');
+
+		$clearBtn = $console.find('button.btn-clear');
+		$searchBtn = $console.find('button.btn-search');
+		$exportBtn = $console.find('button.btn-export');
+
+		$clearBtn.on('click', onClearMap);
+		$clearBtn.on('click', onExportMap);
+
+		$console.on('submit', onConsoleSubmit);
+		$search.on('submit', onSearchSubmit);
+
+		var mapOptions = {
+			center: new google.maps.LatLng(46.8032826, -71.242796),
+			zoom: 17,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		map = new google.maps.Map($map.get(0), mapOptions);
+
+		geocoder = new google.maps.Geocoder();
+
+		google.maps.event.addListener(map, 'center_changed', onMapChange);
+		google.maps.event.addListener(map, 'zoom_changed', onMapChange);
+
+		google.maps.event.addListenerOnce(map, 'idle', function () {
+			google.maps.event.trigger(map, 'zoom_changed');
+		});
+	}
+
+	var onExportMap = function (ev) {
+		ev.preventDefault();
+
+	}
+
+	var onClearMap = function (ev) {
+		ev.preventDefault();
+
+		$.each(markers, function (i, marker) {
+			marker.setMap(null);
+		});
+
+		markers = [];
+
+		$clearBtn.attr('disabled', true);
+		$exportBtn.attr('disabled', true);
+	}
+
+	var onConsoleSubmit = function (ev) {
+		ev.preventDefault();
+
+		$clearBtn.trigger('click');
+
+		var $form = $(this);
+
+		$.ajax({
+			url: $form.attr('action'),
+			type: $form.attr('method'),
+			data: $form.serializeArray(),
+			dataType: 'json',
+			success: function (data) {
+				$clearBtn.attr('disabled', false);
+				$exportBtn.attr('disabled', false);
+
+				onDataReceived(data);
+			},
+			complete: function () {
+				$form.find(':input').attr('disabled', false);
+			},
+			error: function (request, error, message) {
+				console.log(request, error, message);
+			}
+		});
+
+		$form.find(':input').attr('disabled', true);
+	}
+
+	var onDataReceived = function (data) {
+		// Loop through intersections to display markers
+		$.each(data, function (i, intersection) {
+			// Create marker
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(intersection.lat, intersection.lng),
+				map: map,
+				title: intersection.id,
+				icon:createMarker()
+			});
+
+			google.maps.event.addListener(marker, 'click', onMarkerClick);
+
+			// Store marker in array
+			markers.push(marker);
+		});
+	}
+
+	var onMapChange = function (ev) {
+		// Get bounds
+		var bounds = map.getBounds();
+
+		// Change bounds
+		$console.find('input#maxlon').val(map.getBounds().getNorthEast().lng());
+		$console.find('input#minlon').val(map.getBounds().getSouthWest().lng());
+		$console.find('input#maxlat').val(map.getBounds().getNorthEast().lat());
+		$console.find('input#minlat').val(map.getBounds().getSouthWest().lat());
+	}
+
+	var onSearchSubmit = function (ev) {
+		ev.preventDefault();
+
+		var $form = $(this);
+		var query = $form.find('input:first').val();
+
+		$form.find(':input').attr('disabled', true);
+
+		geocoder.geocode({'address': query}, function (results, status) {
+			$form.find(':input').attr('disabled', false);
+
+			if (status == google.maps.GeocoderStatus.OK) {
+				map.setCenter(results[0].geometry.location);
+			}
+		});
+	}
+	
+	var rad = function rad(x) {
+		return x*Math.PI/180
+	};
+		
+	var resetMarkers = function() {
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setIcon(createMarker());
+		}
+	}
+
+	var onMarkerClick = function (ev, marker) {
+		resetMarkers();
+		
+		var currentMarker = this;
+		
+		currentMarker.setIcon(createMarker('00FF00'));
+		
+		var lat = currentMarker.position.lat();
+		var lng = currentMarker.position.lng();
+		var R = 6371; // radius of earth in km
+		var distances = [];
+		var closest = -1;
+		for (var i = 0; i < markers.length; i++) {
+			var marker = markers[i];
+			
+			if (marker.title === currentMarker.title) {
+				continue;
+			}
+			
+			var mlat = marker.position.lat();
+			var mlng = marker.position.lng();
+			var dLat = rad(mlat - lat);
+			var dLong = rad(mlng - lng);
+			var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+				Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			var d = R * c;
+			
+			distances[i] = d;
+			if (closest == -1 || d < distances[closest]) {
+				closest = i;
+			}
+		}
+		
+		markers[closest].setIcon(createMarker('0000FF'));
+
+//		alert(markers[closest].title);
+	}
+	
+	var createMarker = function(color) {
+		return new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + (color || 'FE7569'),
+		        new google.maps.Size(21, 34),
+		        new google.maps.Point(0,0),
+		        new google.maps.Point(10, 34));
+	}
+
+	var construct = (function () {
+		google.maps.event.addDomListener(window, 'load', onAppReady);
+	})();
+
+})();
---
+++ search.php
@@ -0,0 +1,95 @@
+<?php
+set_time_limit(60);
+
+function filter_occurences($var) {
+	if ($var >= 2) {
+		return $var;
+	}
+	
+	return false;
+}
+
+include_once('uagent.php');
+
+if (empty($_POST)) {
+	$_POST['authenticity_token'] = 'lkmiXfMm+6KIyOcBXojI9BWLyEk+V57aU9Dv3QsRbHE=';
+	$_POST['commit'] = 'Export';
+	$_POST['format'] = 'osm';
+
+	$_POST['maxlon'] = '-71.22261';
+	$_POST['minlon'] = '-71.23308';
+
+	$_POST['minlat'] = '44.80521';
+	$_POST['maxlat'] = '46.81091';
+}
+
+$hash = md5(json_encode($_POST));
+$xml_file = sprintf('data/%s.xml', $hash);
+$json_file = sprintf('data/%s.json', $hash);
+
+$ch = curl_init('http://www.openstreetmap.org/export/finish');
+
+$user_agents = array(
+	'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0',
+	'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0',
+	'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0',
+	'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
+	'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+);
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_USERAGENT, $user_agents[array_rand($user_agents)]);
+
+$response = curl_exec($ch);
+
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+
+curl_close($ch);
+
+file_put_contents($xml_file, $body);
+
+$xml = simplexml_load_string($body);
+
+// Exclude ways with buildings
+$nodes = $xml->xpath('/osm/way[not(tag[@k="building" or @k="building:part" or @k="amenity" or @k="historic" or @k="natural" or @k="waterway" or @k="leisure" or @k="bridge" or @k="railway" or @k="service" or @v="footway" or @v="industrial" or @v="recreation_ground" or @v="dyke" or @v="cycleway" or @v="pedestrian" or @v="track" or @v="grass" or @v="cemetery"])]/nd');
+
+// Prepare array
+$nodeIds = array();
+
+// Build simpler array to search for duplicates
+foreach ($nodes as $node) {
+	$nodeIds[] = (string)$node->attributes()->ref;
+}
+
+$nodeIds = array_keys(array_filter(array_count_values($nodeIds), 'filter_occurences'));
+$nodeIds = array_combine($nodeIds, $nodeIds);
+
+$intersections = array();
+
+foreach($xml->xpath('/osm/node') as $node) {
+	$attributes = $node->attributes();
+	
+	if (isset($nodeIds[(string)$attributes->id])) {
+		$intersections[] = array(
+			'id' => (string)$attributes->id,
+			'lat' => (string)$attributes->lat,
+			'lng' => (string)$attributes->lon,
+		);
+	}
+}
+
+header('Content-type: application/json');
+
+$json = json_encode($intersections);
+
+file_put_contents($json_file, $json);
+
+echo $json;
