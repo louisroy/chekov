4dac6391152588b0f14958989589cbc783fa14bd
Louis Roy <louis.roy@cossette.com>
2014-08-19 11:18:49 -0400

    removed useless function

--- javascript/js/app.js
+++ javascript/js/app.js
@@ -418,24 +418,3 @@ var App = (function () {
 
 	return self;
 })();
-
-/**
- * Transforms form data into an object via serializeArray
- * Solution by Tobias Cohen (http://stackoverflow.com/a/1186309/176772)
- * @returns {Object}
- */
-$.fn.serializeObject = function () {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function () {
-		if (o[this.name] !== undefined) {
-			if (!o[this.name].push) {
-				o[this.name] = [o[this.name]];
-			}
-			o[this.name].push(this.value || '');
-		} else {
-			o[this.name] = this.value || '';
-		}
-	});
-	return o;
-};
